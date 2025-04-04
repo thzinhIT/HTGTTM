cb973e6967dd643148076544cc8cba752777cec9
Đặng Minh Tiến <kiritanitaiyo@gmail.com>
2025-04-04 20:46:52 +0700

    Update route.js

--- bikerental/src/app/api/auth/users/addUser/route.js
+++ bikerental/src/app/api/auth/users/addUser/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
 
         await pool.execute(
             "INSERT INTO users (email, password, username, phone) VALUES (?, ?, ?, ?)",
-            [email, hashedPassword, username, phone, userRole]
+            [email, hashedPassword, username, phone]
         );
 
         const token = jwt.sign({ email, username, password }, "mysecretkey", { expiresIn: "1h" });
