d12124b642e2b0f954381b434ee1334860d381bc
Thành Vinh <vinhthanhnguyen12122004@gmail.com>
2025-04-06 13:18:23 +0700

    chuẩn bị layout admin

---
+++ bikerental/src/app/admin/layout.js
@@ -0,0 +1,20 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import React from "react";
+import { Sidebar } from "@/components/ui/sidebar";
+export const metadata = {
+  title: "Admin Panel",
+  description: "Quản trị Bike Rental",
+};
+
+export default function Layout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
